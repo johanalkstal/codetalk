5eae13c03a1aa2caed0647ca5c20d0685e847291
Johan Alkstål <hi@johanalkstal.com>
2019-04-13 20:06:38 +0200

    Remove all things React

--- src/pages/home.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Home() {
-  return <main className="p-2" />;
-}
-
-export default Home;
--- src/pages/sign-out.js
+++
@@ -1,7 +0,0 @@
-import React, { useEffect } from "react";
-import { signOut } from "../core/firebase-auth";
-
-export default function SignOut() {
-  useEffect(() => signOut(), []);
-  return <p>Signing you out...</p>;
-}
